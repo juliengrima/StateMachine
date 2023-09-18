d56d69117a8a8ef78a2ecb6e392611b4ffb2abf3
juliengrima <gogrinformatique@gmail.com>
2023-09-18 12:22:15 +0200

    fix name detection scrpits

--- Assets/Scripts/Dectection.cs
+++ Assets/Scripts/Dectection.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Exit : MonoBehaviour
+public class Dectection : MonoBehaviour
 {
     #region Champs
     #endregion
